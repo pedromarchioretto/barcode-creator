a71fff635af379e85dfa6dc42f02c4cbcb9c2efc
Pedro Marchioretto <pepepaiva221@gmail.com>
2024-02-07 12:34:24 -0300

    Feat: distribuing the create tag image method throught controllers/views/handlers etc

---
+++ src/controllers/tag_create_controller.py
@@ -0,0 +1,25 @@
+from typing import Dict
+from src.drivers.bar_code_handler import BarCodeHandler
+
+class TagCreateController:
+  '''
+    Responsible to implementing business rules
+  '''
+  def create(self, product_code: str) -> Dict:
+    path_from_tag = self.__create_barcode(product_code)
+    formatted_response = self.__formatted_response(path_from_tag)
+    return formatted_response
+
+  def __create_barcode(self, product_code: str) -> str:
+    barcode_handler = BarCodeHandler()
+    path_from_tag = barcode_handler.create_barcode(product_code)
+    return path_from_tag
+
+  def __formatted_response(self, path_from_tag: str) -> Dict:
+    return{
+      "data":{
+          'Type': 'Tag Image',
+          'count': 1,
+          'path': f"{path_from_tag}.png"
+      }
+    }
--- src/views/tag_create_view.py
+++ src/views/tag_create_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_create_controller import TagCreateController
 
 class TagCreatorView:
   '''
@@ -9,8 +10,8 @@ class TagCreatorView:
     body = http_requests.body
     product_code = body["product_code"]
 
-    print (product_code)
-    #logica de regra de negocios
+    create_controller = TagCreateController()
+    formatted_response = create_controller.create(product_code)
 
     #retorno http
-    return HttpResponse(status_code=200, body={"hello": "world"})
+    return HttpResponse(status_code=200, body=formatted_response)
